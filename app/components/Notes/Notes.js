6c4af6a49fa83a69561d0b30699b9eb5708a22e1
Francisco De La Cruz <helloquico@gmail.com>
2015-12-28 00:41:24 -0500

    Use ReactFire to Add Data  Persistence

--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -1,15 +1,15 @@
 var React = require('react');
+var NotesList = require('./NotesList');
 
 var Notes = React.createClass({
   render: function () {
     return (
       <div>
-        <p> NOTES </p>
-        <p> {this.props.notes} </p>
+        <h3> NOTES for {this.props.username} </h3>
+        <NotesList notes={this.props.notes} />
       </div>
     );
   }
 });
 
 module.exports = Notes;
-
